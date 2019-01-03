671971dbf6aa762feaf00b7b991be109735e034d
Nicole <nicole.sattler@live.com>
2019-01-03 13:05:09 -0600

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp')
+const autoprefixer = require('gulp-autoprefixer')
+const sass = require('gulp-sass')
+const wait = require('gulp-wait')
+
+gulp.task('prefixer', () =>
+  gulp
+    .src('src/main.scss')
+    .pipe(wait(200))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ['last 4 versions']
+      })
+    )
+    .pipe(gulp.dest('dist'))
+)
+
+gulp.task('watch:styles', () => {
+  gulp.watch('./src/**/*.scss', gulp.series('prefixer'))
+})
