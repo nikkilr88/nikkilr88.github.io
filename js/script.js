eff1c6658ba1d12f800ab71735373a4e4ed9c9ea
Nicole <nicole.sattler@live.com>
2019-05-07 15:45:35 -0500

    Refactor and add logic for navbar style change on scroll

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+console.log('running')
+
+const nav = document.querySelector('.navbar')
 const form = document.querySelector('.form')
 const inputs = document.querySelectorAll('.form-input')
 const name = document.querySelector('#name')
@@ -5,14 +8,32 @@ const email = document.querySelector('#email')
 const msg = document.querySelector('#message')
 const displayMsg = document.querySelector('#display-msg')
 
-// Form input shizz
-for (let input of inputs) {
-  input.addEventListener('focusout', handleInputAnimation)
+window.addEventListener('scroll', handleScroll)
+
+function handleScroll(e) {
+  const scrollPos =
+    this.scrollY || this.scrollTop || document.documentElement.scrollTop
+
+  if (scrollPos > 100) {
+    nav.style.padding = '25px'
+    nav.style.boxShadow = 'rgba(0, 0, 0, 0.5) 0px 5px 10px -10px'
+    nav.style.background = '#f9f9f9'
+  } else {
+    nav.style.padding = '50px'
+    nav.style.boxShadow = 'none'
+    nav.style.background = 'none'
+  }
 }
+
+// Form input shizz
+Array.prototype.slice.call(inputs).forEach(function(input) {
+  input.addEventListener('focusout', handleInputAnimation)
+})
 
 function handleInputAnimation(e) {
   const val = e.target.value
 
+  console.log(val)
   if (val) {
     e.target.classList.add('notEmpty')
   } else {
@@ -35,29 +56,26 @@ function handleFormSubmit(e) {
   axios
     .post('https://node-sender.glitch.me/', data)
     .then(function(res) {
-      displayMsg.innerHTML = res.data.success
-        ? `<div class="msg success"><i class="far fa-times-circle"></i><span>${
-            res.data.success
-          }</span></div>`
-        : `<div class="msg error"><i class="far fa-times-circle"></i><span>${
-            res.data.error
-          }</span></div>`
-
+      displayData(res)
+      console.log(res)
       if (res.data.success) {
         resetForm()
       }
     })
     .catch(function(err) {
-      displayMsg.innerHTML = `<div class="msg error"><i class="far fa-times-circle"></i>${err}</div>`
+      displayMsg.innerHTML =
+        '<div class="msg error"><i class="far fa-times-circle"></i>' +
+        err +
+        '</div>'
     })
 }
 
 // Clear out form
 function resetForm() {
-  for (let input of inputs) {
+  Array.prototype.slice.call(inputs).forEach(function(input) {
     input.classList.remove('notEmpty')
     input.value = ''
-  }
+  })
 }
 
 // Display message shizz
@@ -65,4 +83,14 @@ displayMsg.addEventListener('click', hideDisplayMsg)
 
 function hideDisplayMsg() {
   displayMsg.innerHTML = ''
+}
+
+function displayData(res) {
+  displayMsg.innerHTML = res.data.success
+    ? '<div class="msg success"><i class="far fa-times-circle"></i><span>' +
+      res.data.success +
+      '</span></div>'
+    : '<div class="msg error"><i class="far fa-times-circle"></i><span>' +
+      res.data.error +
+      '</span></div>'
 }
