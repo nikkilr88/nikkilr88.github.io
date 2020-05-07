d044fd704f61a46eafecb7cc0462e5dca7c9b220
Nicole Sattler <nicole.sattler@live.com>
2020-05-07 12:36:33 -0500

    Add gulpfile back in, add display swap for Google fonts

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp')
+const wait = require('gulp-wait')
+const sass = require('gulp-sass')
+const purgecss = require('gulp-purgecss')
+const minifyCSS = require('gulp-minify-css')
+const autoprefixer = require('gulp-autoprefixer')
+
+gulp.task('sass', () =>
+  gulp
+    .src('src/main.scss')
+    .pipe(wait(350))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(
+      autoprefixer({
+        browserList: ['defaults', 'last 4 versions'],
+      })
+    )
+    .pipe(
+      purgecss({
+        content: ['index.html'],
+      })
+    )
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('css'))
+)
+
+gulp.task('watch:sass', () => {
+  gulp.watch('./src/**/*.scss', gulp.series('sass'))
+})
